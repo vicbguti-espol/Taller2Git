d365ea0196e109766cfcde1daa152fcc7e7392cf
Izaako04 <isaac.criollo04@gmail.com>
2023-10-27 20:21:10 -0500

    Mayonnaise addition

--- Hamburger/src/Hamburger/HealthyBurger.java
+++ Hamburger/src/Hamburger/HealthyBurger.java
@@ -11,6 +11,8 @@ public class HealthyBurger extends BaseBurger {
         System.out.println("Mustard");
         additions.add(6);
         System.out.println("Ketchup");
+        additions.add(8);
+        System.out.println("Mayonnaise");
 
     }
 
